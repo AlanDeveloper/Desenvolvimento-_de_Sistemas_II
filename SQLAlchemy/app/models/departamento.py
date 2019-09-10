c3a79e4d53e8ab45ca0de80bbd11a10c2c1ed5fc
Alan Santos <alanssantos32@gmail.com>
2019-09-10 12:10:14 -0300

    Update departamento.py

--- SQLAlchemy/app/models/departamento.py
+++ SQLAlchemy/app/models/departamento.py
@@ -5,7 +5,7 @@ class departamento(db.Model):
     __tablename__ = 'departamento'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    data_atualizacao = db.Column(db.DateTime, unique=True, default=db.func.now())
+    data_atualizacao = db.Column(db.DateTime, unique=True, default=db.func.now(), onupdate= db.func.now())
 
     def __init__(self, nome):
         self.nome = nome
